66add904f3310771f115e5c92ac39f03ea510a56
ErayC4 <eray04.coskun@gmail.com>
2024-10-17 17:43:31 +0200

    timetable page styling reworked

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :user
+  before_create :assign_random_color
+
+  private
+
+  def assign_random_color
+    colors = Array["#E1D2FF", "#FDE1AC", "#BAE5F5", "#CCEFBF"]
+    random_number = rand(0..3) 
+    self.color = colors[random_number]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_14_182744) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_17_130941) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "notes"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_14_182744) do
     t.string "subtask_title"
     t.string "subtask_left_of_at"
     t.boolean "subtask_finished"
+    t.string "color"
     t.index ["subtask_id"], name: "index_tasks_on_subtask_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
